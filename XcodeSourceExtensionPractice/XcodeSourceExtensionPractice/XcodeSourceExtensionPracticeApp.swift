ce5b1989871a6f7a0a9fb076d1e2ca2f3562a985
Sarah Clark <sarahireneclark@gmail.com>
2024-03-16 13:25:28 -0500

    Initial commit of project and source editor extension target

---
+++ XcodeSourceExtensionPractice/XcodeSourceExtensionPractice/XcodeSourceExtensionPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeSourceExtensionPracticeApp.swift
+//  XcodeSourceExtensionPractice
+//
+//  Created by Sarah Clark on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeSourceExtensionPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
